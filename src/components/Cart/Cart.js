6f48f02d454dcc7d7460c0cbdd627f0f6a2a94d2
Paritosh Biswas <53013257+ParitoshBiswas@users.noreply.github.com>
2023-02-26 12:06:16 +0530

    Made Add And Remove Button Functional In Cart Modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,18 +10,23 @@ const Cart = props => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemAddHandler = (item) => { };
-    const cartItemRemoveHandler = (id) => { };
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({...item, amount: 1});
+    };
 
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map((item) => <CartItem
                 key={item.id}
                 name={item.name}
-                price={item.price}
                 amount={item.amount}
-                onAdd={cartItemAddHandler.bind(null, item)}
+                price={item.price}
                 onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                onAdd={cartItemAddHandler.bind(null, item)}
             />
             )}
         </ul>
